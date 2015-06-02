8fd378f47cc19da6bddb073278a5d32863200e35
hinest <treverhines@gmail.com>
2015-06-02 16:48:29 -0400

    stable

--- PyPGI.py
+++ PyPGI.py
@@ -1,11 +1,81 @@
 #!/usr/bin/env python
+#
+# This function is intended to check to validity of the input and 
+# handle output 
+#
 import sys
 sys.path.append('./lib')
-sys.path.append('./input')
 import argparse
 import pypgi
+import h5py
+import json
+import logging
+import misc
 
+# Setup command line argument parser
 p = argparse.ArgumentParser(
       description='Python-based Postseismic Geodetic Inverstion')
-args = p.parse_args()
-pypgi.main()
+
+args = vars(p.parse_args())
+
+# Setup logger
+logger = logging.getLogger()
+formatter = logging.Formatter(
+              '%(asctime)s %(module)s: [%(levelname)s] %(message)s',
+              '%m/%d/%Y %H:%M:%S')
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(formatter)
+#file_handler = logging.FileHandler(
+#                 'output/%s/%s.log' % (name,name),'w')
+#file_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.INFO)
+
+# Load input files
+data_file = h5py.File('input/data.h5')
+data = pypgi.hdf5_to_dict(data_file)
+data_file.close()
+
+gf_file = h5py.File('input/greens_functions.h5')
+gf = pypgi.hdf5_to_dict(gf_file)
+gf_file.close()
+
+param_file = open('input/usrparam.json')
+param = json.load(param_file)
+param_file.close()
+
+try:
+  prior_file = h5py.File('input/prior.h5','r')
+  prior = pypgi.hdf5_to_dict(prior_file)
+  prior_file.close()
+
+except IOError:
+  prior = None
+
+
+try:
+  reg_file = h5py.File('input/regularization.h5','r')
+  reg = pypgi.hdf5_to_dict(reg_file)
+  reg_file.close() 
+
+except IOError:
+  reg = None
+
+out = pypgi.kalmanfilter(data,prior,gf,reg,param)
+
+name = param['name']
+itr = 1
+outname = name
+while True:
+  try:
+    out_file = h5py.File('output/%s.h5' % outname,'w-')
+    break
+
+  except IOError:
+    logger.warning(
+      'output name %s exists, trying %s-%s' % (outname,name,itr))
+    outname = '%s-%s' % (name,itr)
+    itr += 1
+
+pypgi.dict_to_hdf5(out,out_file)
+logger.info('output saved to output/%s.h5' % outname)
--- lib/pypgi.py
+++ lib/pypgi.py
@@ -1,61 +1,186 @@
 #!/usr/bin/env python
-import sys
-import json
-import h5py
 import numpy as np
-import filterfcn
-import regularization
 import inverse
+import logging
+import h5py
+import scipy
 
-def main():
-  try:
-    f = open('input/usrparam.json')
-  except IOError:
-    raise ImportError(
-      'must have a json file named "usrparam.json" within the input ')
-  param = json.load(f)
-  f.close()
+logger = logging.getLogger(__name__)
 
-  try:
-    from usrfcn import slip_basis
-  except ImportError:
-    raise ImportError(
-      'must have a module named "usrfcn.py" within the input '
-      'directory and it must contain the function slip_basis')
+def hdf5_to_dict(f):
+  if type(f) is h5py.File:
+    return hdf5_to_dict(f['/'])
 
-  try:
-    from usrfcn import fluidity_basis
-  except ImportError:
-    raise ImportError(
-      'must have a module named usrfcn.py within the input '
-      'directory and it must contain the function fluidity_basis')
+  if type(f) is h5py.Group:
+    return {i:hdf5_to_dict(v) for i,v in f.iteritems()}
 
-  try:
-    f = h5py.File('input/greens_functions.h5','r')
-  except IOError:
-    raise IOError(
-      'must have a h5py file named greens_functions.h5 within the '
-      'input directory')
+  if type(f) is h5py.Dataset:
+    return np.array(f)
 
-  try:
-    F = f['elastic'][...]
-    G = f['viscoelastic'][...]
-    x = f['metadata/position'][...]
-  except KeyError:
-    raise KeyError
 
-  f.close()
-  try:
-    f = h5py.File('input/data.h5','r')
-  except IOError:
-    raise IOError(
-      'must have a h5py file named data.h5 within the input directory')
+def dict_to_hdf5(d,f):
+  for i,v in d.iteritems():
+    if type(v) is dict:
+      f.create_group(i) 
+      dict_to_hdf5(v,f[i])
 
+    else:   
+      f.create_dataset(i,data=v)
+
+
+def state_parser(Ns,Ds,Nv,Nx,Dx):
+  out = {}
+
+  out['Ns'] = Ns
+  out['Ds'] = Ds
+  out['Nv'] = Nv
+  out['Nx'] = Nx
+  out['Dx'] = Dx
+  out['total'] = 3*Ns*Ds + Nv + 2*Nx*Dx
+
+  slip_integral = range(0,Ns*Ds)
+  out['slip_integral'] = np.reshape(slip_integral,(Ns,Ds))
+
+  slip = range(Ns*Ds,2*Ns*Ds)
+  out['slip'] = np.reshape(slip,(Ns,Ds))
+
+  slip_derivative = range(2*Ns*Ds,3*Ns*Ds)
+  out['slip_derivative'] = np.reshape(slip_derivative,(Ns,Ds))
+
+  out['fluidity'] = range(3*Ns*Ds,3*Ns*Ds + Nv)
+
+  secular_velocity = range(3*Ns*Ds + Nv,3*Ns*Ds + Nv + Nx*Dx)
+  out['secular_velocity'] = np.reshape(secular_velocity,(Nx,Dx))
+
+  baseline = range(3*Ns*Ds + Nv + Nx*Dx,3*Ns*Ds + Nv + 2*Nx*Dx)
+  out['baseline_displacement'] = np.reshape(baseline,(Nx,Dx))
+
+  return out
+
+
+def flat_data(u):
+  '''                                                                                                   
+  takes a Nx by Dx array of data and flattens it to one dimension   
+  '''
+  return np.reshape(u,(np.prod(np.shape(u)),))
+
+
+def flat_covariance(C):
+  '''                                                                                                   
+  takes a Nx by Dx by Dx covariance array and flattens it to two  
+  dimension             
+  '''
+  return scipy.linalg.block_diag(*C)
+
+
+def observation(X,t,F,G,p,flatten=True):
+  tect = X[p['secular_velocity']]*t + X[p['baseline_displacement']]
+  slip = np.einsum('ijkl,ij',F,X[p['slip']])
+  visc = np.einsum('ijklm,ij,k',G,X[p['slip_integral']],
+                                  X[p['fluidity']])
+  out = tect + slip + visc
+
+  if flatten:
+    out = flat_data(out)
+
+  return out
+
+
+def transition(X,t2,t1,p):
+  Xout = np.zeros(p['total'])
+  dt = t2 - t1
+  Xout[p['slip_integral']] += (X[p['slip_integral']] +
+                               X[p['slip']]*dt +
+                               X[p['slip_derivative']]*0.5*dt**2)
+
+  Xout[p['slip']] += (X[p['slip']] +
+                      X[p['slip_derivative']]*dt)
+
+  Xout[p['slip_derivative']] += X[p['slip_derivative']]
+
+  Xout[p['fluidity']] += X[p['fluidity']]
+
+  Xout[p['secular_velocity']] += X[p['secular_velocity']]
+
+  Xout[p['baseline_displacement']] += X[p['baseline_displacement']]
+
+  return Xout
+
+
+def process_covariance(t2,t1,alpha,p):
+  dt = t2 - t1
+  Q = np.zeros((p['total'],p['total']))
+
+  Q[p['slip_derivative'],p['slip_derivative']] = alpha**2*dt
+  Q[p['slip'],p['slip']] = alpha**2*(dt**3)/3.0
+  Q[p['slip_integral'],p['slip_integral']] = alpha**2*(dt**5)/20.0
+
+  Q[p['slip_integral'],p['slip']] = alpha**2*(dt**4)/8.0
+  Q[p['slip'],p['slip_integral']] = alpha**2*(dt**4)/8.0
+
+  Q[p['slip_derivative'],p['slip']] = alpha**2*(dt**2)/2.0
+  Q[p['slip'],p['slip_derivative']] = alpha**2*(dt**2)/2.0
+
+  Q[p['slip_derivative'],p['slip_integral']] = alpha**2*(dt**3)/6.0
+  Q[p['slip_integral'],p['slip_derivative']] = alpha**2*(dt**3)/6.0
+
+  return Q
+
+
+def kalmanfilter(data,prior,gf,reg,param):
+  '''
+  Nt: number of time steps
+  Nx: number of positions
+  Dx: spatial dimensions of coordinates and displacements 
+  Ns: number of slip basis functions per slip direction
+  Ds: number of slip directions
+  Nv: number of fluidity basis functions
+  total: number of state parameters (Ns*Ds + Nv + 2*Nx*Dx)
+
+  Parameters
+  ----------
+    data: \ mean : (Nt,Nx,Dx) array
+          \ covariance : (Nt,Nx,Dx,Dx) array
+          \ metadata \ position : (Nx,Dx) array
+                       time : (Nt,) array
+
+    prior: \ mean : (total,) array
+           \ covariance : (total,total) array
+
+    gf: \ elastic : (Ns,Ds,Nx,Dx) array
+        \ viscoelastic : (Ns,Ds,Dv,Nx,Dx) array
+        \ metadata \ position : (Nx,Dx) array
+
+    reg: \ regularization : (*,total) array
+
+    params: user parameter dictionary
+
+    
+
+  Returns
+  -------
+    out: \ slip_integral \ mean : (Nt,Ns,Ds) array
+                         \ uncertainty :(Nt,Ns,Ds) array
+         \ slip \ mean : (Nt,Ns,Ds) array
+                \ uncertainty : (Nt,Ns,Ds) array
+         \ slip_derivative \ mean : (Nt,Ns,Ds) array
+                           \ uncertainty : (Nt,Ns,Ds) array
+         \ fluidity \ mean : (Nv,) array
+                    \ uncertainty : (Nv,) array
+         \ secular_velocity \ mean : (Nx,Dx) array
+                            \ uncertainty : (Nx,Dx) array
+         \ baseline_displacement \ mean : (Nx,Dx) array
+                                 \ uncertainty : (Nx,Dx) array
+
+  '''
+  F = gf['elastic']
+  G = gf['viscoelastic']
+  x = gf['metadata']['position']
   alpha = param['slip_acceleration_variance']
-  u = f['data'][...]
-  Cd = f['covariance'][...]
-  x2 = f['metadata/position'][...]
-  t = f['metadata/time'][...]
+  u = data['mean']
+  Cd = data['covariance']
+  x2 = data['metadata']['position']
+  t = data['metadata']['time']
 
   # check for consistency between input
   Ns,Ds,Nv,Nx,Dx = np.shape(G)
@@ -65,31 +190,75 @@ def main():
   assert np.shape(Cd) == (Nt,Nx,Dx,Dx)
   assert np.shape(F) == (Ns,Ds,Nx,Dx)
   assert np.shape(G) == (Ns,Ds,Nv,Nx,Dx)
+  p = state_parser(Ns,Ds,Nv,Nx,Dx)
+  
+  if reg is None:
+    reg1 = inverse.tikhonov_matrix(p['slip'],0,column_no=p['total'])
+    reg2 = inverse.tikhonov_matrix(p['fluidity'],0,column_no=p['total'])
+    regmat = np.vstack((reg1,reg2))
 
-  p = filterfcn.state_parser(Ns,Ds,Nv,Nx,Dx)
+  else:
+    regmat = reg['regularization']
 
-  Xprior,Cprior = regularization.priors(p)
+  if prior is None:
+    Xprior = np.zeros(p['total'])  
+    Cprior = 1e6*np.eye(p['total'])  
 
-  reg = regularization.regularization_matrix(param,p)
+  else:
+    Xprior = prior['mean']
+    Cprior = prior['covariance']
 
   kalman = inverse.KalmanFilter(Xprior,Cprior,
-                                filterfcn.transition,
-                                filterfcn.observation,
-                                filterfcn.process_covariance)
+                                transition,
+                                observation,
+                                process_covariance)
+
+  logger.info('starting Kalman filter iterations')
   for i in range(Nt):
-    flat_ui = filterfcn.flat_data(u[i,...])
-    flat_Cdi = filterfcn.flat_covariance(Cd[i,...])
+    flat_ui = flat_data(u[i,...])
+    flat_Cdi = flat_covariance(Cd[i,...])
     kalman.update(flat_ui,flat_Cdi,
                   observation_args=(t[i],F,G,p),
-                  solver_kwargs={'regularization':reg})
+                  solver_kwargs={'regularization':regmat,
+                                 'LM_damping':False})
     if i != (Nt - 1):
       kalman.predict(transition_args=(t[i+1],t[i],p),
                      process_covariance_args=(t[i+1],t[i],alpha,p))   
 
+    logger.info('finished Kalman filter iteration %s of %s' % (i+1,Nt))
+
+  logger.info('starting RTS smoothing')
   kalman.smooth()
-  
-  print(kalman.state[0]['smooth'][p['fluidity']])
-  print(kalman.state[0]['smooth'][p['slip']])
-  return
+
+  state_mean = kalman.get('smooth')
+  state_covariance = kalman.get('smooth_covariance')
+  state_std = [np.sqrt(np.diag(i)) for i in state_covariance]
+
+  slip_int = {'mean':np.array([i[p['slip_integral']] for i in state_mean]),   
+              'uncertainty':np.array([i[p['slip_integral']] for i in state_std])}
+
+  slip = {'mean':np.array([i[p['slip']] for i in state_mean]),   
+          'uncertainty':np.array([i[p['slip']] for i in state_std])}
+
+  slip_dif = {'mean':np.array([i[p['slip_derivative']] for i in state_mean]),   
+              'uncertainty':np.array([i[p['slip_derivative']] for i in state_std])}
+
+  visc = {'mean':state_mean[-1][p['slip']],   
+          'uncertainty':state_std[-1][p['slip']]}
+
+  vel = {'mean':state_mean[-1][p['secular_velocity']],   
+         'uncertainty':state_std[-1][p['secular_velocity']]}
+
+  base = {'mean':state_mean[-1][p['baseline_displacement']],   
+          'uncertainty':state_std[-1][p['baseline_displacement']]}
+
+  out = {'slip_integral':slip_int,
+         'slip':slip,
+         'slip_derivative':slip_dif,
+         'visc':visc,
+         'secular_velocity':vel,
+         'baseline_displacement':base}
+
+  return out
 
 
